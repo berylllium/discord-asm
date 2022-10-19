bdf570cf38098b1fab0c569c7b73f4bda3bc8046
BerryHeyy <berkaykaplanbk@hotmail.com>
2022-10-19 09:35:19 +0200

    Stackpointer should start at 0

--- src/processor.hpp
+++ src/processor.hpp
@@ -70,7 +70,7 @@ public:
     void reset()
     {
         PC = 0x0200;
-        SP.value = 0x0100;
+        SP.value = 0x0000;
         R8 = R9 = R10 = R11 = R12 = R13 = R14 = R15 = 0;
         CF = 0;
     }
